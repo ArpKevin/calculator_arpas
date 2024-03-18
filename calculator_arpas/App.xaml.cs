0478c0428c6b16f5ac7434dde8bf86cf9419dc52
Kevin <arpaskevin11@gmail.com>
2024-03-18 14:05:26 +0100

    Add project files.

---
+++ calculator_arpas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace calculator_arpas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ calculator_arpas/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculator_arpas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic for equals button click
+        }
+
+        private void btnClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic for clear entry button click
+        }
+
+        private void regularButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Implement logic for regular button click
+        }
+
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic for dot button click
+        }
+
+        private void operationButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic for operation button click
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic for back button click
+        }
+
+        private void btnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic for clear all button click
+        }
+    }
+}
