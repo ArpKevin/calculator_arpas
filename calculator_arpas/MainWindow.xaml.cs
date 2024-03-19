132e5d7fa3b8c92983acdbbd1876e7f05c960da5
Kevin <arpaskevin11@gmail.com>
2024-03-19 10:23:16 +0100

    Some calculator functionalities added. -Division by 0 to be handled. -Brackets -potentially- to be added (multiplication with negative values is currently impossible due to the operation replace of operationButton_Click). -Additional operations such as Square Root and Power to be added. -UI to be changed in accordance with the buttons to-be-added.

--- calculator_arpas/MainWindow.xaml.cs
+++ calculator_arpas/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,37 +24,110 @@ namespace calculator_arpas
 
         private void btnEquals_Click(object sender, RoutedEventArgs e)
         {
-            // Implement logic for equals button click
+            // Get the expression from the text display
+            string expression = txtDisplay.Text;
+
+            // Try to evaluate the expression
+            try
+            {
+                // Evaluate the expression using DataTable Compute method
+                var result = new DataTable().Compute(expression, null);
+
+                // Display the result
+                txtInput.Text = result.ToString();
+            }
+            catch (Exception)
+            {
+                // If an exception occurs during evaluation, display "Invalid" in the text display
+                txtInput.Text = "Invalid";
+            }
         }
 
-        private void btnClearEntry_Click(object sender, RoutedEventArgs e)
-        {
-            // Implement logic for clear entry button click
-        }
 
         private void regularButtonClick(object sender, RoutedEventArgs e)
         {
-            // Implement logic for regular button click
+            Button button = sender as Button;
+
+            if (button != null)
+            {
+                string numberClicked = button.Content.ToString();
+
+                if (txtDisplay.Text == "0") txtDisplay.Text = numberClicked; else txtDisplay.Text += numberClicked;
+            }
         }
+
 
         private void btnDot_Click(object sender, RoutedEventArgs e)
         {
-            // Implement logic for dot button click
+            // Check if the current text already contains a dot
+            if (!txtDisplay.Text.Contains(".")) txtDisplay.Text += ".";
         }
 
+        // Event handler for operation buttons
         private void operationButton_Click(object sender, RoutedEventArgs e)
         {
-            // Implement logic for operation button click
+            Button button = sender as Button;
+
+            if (button != null)
+            {
+                string newOperation = button.Content.ToString();
+
+                // Check if the current display text ends with an operator
+                if (txtDisplay.Text.Length > 0 && IsOperator(txtDisplay.Text[^1]))
+                {
+                    // Replace the existing operator with the new one
+                    txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1) + newOperation;
+                }
+                else txtDisplay.Text += newOperation;
+            }
         }
+
+        // Method to check if a character is an operator
+        private bool IsOperator(char c)
+        {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+
+
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
-            // Implement logic for back button click
+            if (!string.IsNullOrEmpty(txtDisplay.Text))
+            {
+                txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1);
+            }
         }
 
         private void btnClearAll_Click(object sender, RoutedEventArgs e)
         {
-            // Implement logic for clear all button click
+            txtDisplay.Text = "0";
         }
+
+
+
+        private void btnClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtDisplay.Text))
+            {
+                // Find the index of the last operator using LINQ
+                int lastOperatorIndex = txtDisplay.Text.Select((c, index) => new { Char = c, Index = index })
+                                                        .Where(pair => IsOperator(pair.Char))
+                                                        .Select(pair => pair.Index)
+                                                        .DefaultIfEmpty(-1)
+                                                        .Last();
+
+                // If an operator is found, remove everything after it
+                if (lastOperatorIndex >= 0)
+                {
+                    txtDisplay.Text = txtDisplay.Text.Substring(0, lastOperatorIndex);
+                }
+                else
+                {
+                    // If no operator is found, simply clear the display
+                    txtDisplay.Text = "0";
+                }
+            }
+        }
+
     }
 }
