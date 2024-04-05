743362393eba8bff2a3c06e6adcc834a07eb59fd
Kevin <arpaskevin11@gmail.com>
2024-04-05 08:18:09 +0200

    Documentation added to the source code.

--- calculator_arpas/MainWindow.xaml.cs
+++ calculator_arpas/MainWindow.xaml.cs
@@ -24,13 +24,16 @@ namespace calculator_arpas
 
         private void btnEquals_Click(object sender, RoutedEventArgs e)
         {
+            //gomb megnyomására változóba helyezi a beírt karakterek tönkelegét, s megpróbálja kiszámolni az értékét
             string expression = txtDisplay.Text;
 
+            //nullával való osztály ellenőrzése
             if (expression.Contains("/0") && !expression.Contains("0.")) txtInput.Text = "Division by zero was attempted";
             else
             {
                 try
                 {
+                    //automatikusan megpróbálja kiszámolni a beírt kifejezés értékét
                     var result = new DataTable().Compute(expression, null);
                     txtInput.Text = result.ToString();
 
@@ -43,6 +46,7 @@ namespace calculator_arpas
 
         }
 
+        //kinyeri a gomb tartalmából a szám értékét, majd hozzáadja a számológép kijelzőjéhez
         private void regularButtonClick(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
@@ -55,20 +59,23 @@ namespace calculator_arpas
             }
         }
 
-
+        //megnézi hogy már van-e pont a kifejezés végén, ha nincs, ad hozzá egyet a "." gomb megnyomására
         private void btnDot_Click(object sender, RoutedEventArgs e)
         {
-            if (!txtDisplay.Text.Contains(".")) txtDisplay.Text += ".";
+            if (!txtDisplay.Text.EndsWith(".")) txtDisplay.Text += ".";
         }
 
         private void operationButton_Click(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
 
+            // ellenőrzi, hogy az utolsó karakter operátor-e, ha igen, cserélje ki, ellenkező esetben fűzze hozzá
+
             if (button != null)
             {
                 string newOperation = button.Content.ToString();
 
+                //IsOperator = bool típusú metódus, ami megállapítja, hogy operátor-e az utolsó karakter
                 if (txtDisplay.Text.Length > 0 && IsOperator(txtDisplay.Text[^1]))
                 {
                     txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1) + newOperation;
@@ -83,7 +90,7 @@ namespace calculator_arpas
         }
 
 
-
+        //megnézi, hogy üres-e a kijelző, ha nem, lecsépeli az utolsó karaktert
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrEmpty(txtDisplay.Text))
@@ -92,23 +99,26 @@ namespace calculator_arpas
             }
         }
 
+        //0-vá teszi egyenlővé a kifejezést, alapértelmezett helyzetbe visszahelyezi
         private void btnClearAll_Click(object sender, RoutedEventArgs e)
         {
             txtDisplay.Text = "0";
         }
 
 
-
+        //műveleti egységenként töröl ki a kifejezésből karakterenként helyett
         private void btnClearEntry_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrEmpty(txtDisplay.Text))
             {
+                //linq segítségével a szövegből kiválasztja a karaktereket és az indexüket, anonym típusú objektumot hoz létre, amely minden karakterhez hozzárendeli önmagát és az indexét, majd ebből ha operátor a karakter értéke, kiválasztom az indexét, különben ha ilyen nincs, az alapértelmezett érték -1, s kiválasztom az utolsó ilyen karaktert mert addig fog kelleni kitörölni a műveleti egységet
                 int lastOperatorIndex = txtDisplay.Text.Select((c, index) => new { Char = c, Index = index })
                                                         .Where(pair => IsOperator(pair.Char))
                                                         .Select(pair => pair.Index)
                                                         .DefaultIfEmpty(-1)
                                                         .Last();
 
+                //megnézem, hogy volt-e operátor a kifejezésben, ha volt, a változóba kinyert operátor mentén leszeli a kifejezést, különben lenullázza
                 if (lastOperatorIndex >= 0)
                 {
                     txtDisplay.Text = txtDisplay.Text.Substring(0, lastOperatorIndex);
@@ -120,6 +130,7 @@ namespace calculator_arpas
             }
         }
 
+        //gomb nyomására megjeleníti a gyökvonást és a hatványozás gombjait
         private void btnAdvanced_Click(object sender, RoutedEventArgs e)
         {
             btnRoot.Visibility = Visibility.Visible;
